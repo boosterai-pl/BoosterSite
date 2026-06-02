061d9fa2e2f866ac827a226235d24ed3ae7f3a2f
Andrzej Chmielewski <andrzejchm@gmail.com>
2026-06-02 20:37:48 +0200

    Remove full-schema migration files

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260602_150542 from './20260602_150542';
-
-export const migrations = [
-  {
-    up: migration_20260602_150542.up,
-    down: migration_20260602_150542.down,
-    name: '20260602_150542'
-  },
-];
